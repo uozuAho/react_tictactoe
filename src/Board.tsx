53902ba24c6704949b06bc703735cebd19b2944a
Warwick Stone <uozu.aho@gmail.com>
2018-11-17 20:18:45 +1100

    move state to game, add history

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,45 +1,28 @@
 import * as React from 'react';
 import { Square } from './Square';
 
-interface IBoardState {
-    isXnext: boolean;
+interface IBoardProps {
     squares: string[];
+    onClick: (i: number) => void;
 }
 
-export class Board extends React.Component<{}, IBoardState> {
-
-    constructor(props: {}) {
-        super(props);
-        this.state = {
-            isXnext: true,
-            squares: Array(9).fill(null),
-        };
-    }
+export class Board extends React.Component<IBoardProps> {
 
     public renderSquare(i: number) {
         return <Square
-                    value={this.state.squares[i]}
+                    value={this.props.squares[i]}
                     // note: using a lambda here is bad for performance,
                     // but there really doesn't seem to be any other way.
                     // Options:
                     // - extract handler to square component (then how does board know which square was clicked?)
                     // - use redux?
                     // tslint:disable-next-line:jsx-no-lambda
-                    onClick={() => this.handleClick(i)} />;
+                    onClick={() => this.props.onClick(i)} />
     }
 
     public render() {
-        const winner = this.calculateWinner(this.state.squares);
-        let status: string;
-        if (winner) {
-            status = 'Winner: ' + winner;
-        } else {
-            status = 'Next player: ' + (this.state.isXnext ? 'X' : 'O');
-        }
-
         return (
             <div>
-                <div className="status">{status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
@@ -57,37 +40,5 @@ export class Board extends React.Component<{}, IBoardState> {
                 </div>
             </div>
         );
-    }
-
-    private handleClick(i: number) {
-        const squares = this.state.squares.slice();
-        if (this.calculateWinner(squares) || squares[i]) {
-            return;
-        }
-        squares[i] = this.state.isXnext ? 'X' : 'O';
-        this.setState({
-            isXnext: !this.state.isXnext,
-            squares
-        });
-    }
-
-    private calculateWinner(squares: string[]): string | null {
-        const lines = [
-          [0, 1, 2],
-          [3, 4, 5],
-          [6, 7, 8],
-          [0, 3, 6],
-          [1, 4, 7],
-          [2, 5, 8],
-          [0, 4, 8],
-          [2, 4, 6],
-        ];
-        for (const line of lines) {
-          const [a, b, c] = line;
-          if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
-          }
-        }
-        return null;
     }
 }
--- src/Game.tsx
+++ src/Game.tsx
@@ -1,16 +1,84 @@
 import * as React from 'react';
 import { Board } from './Board';
 
-export class Game extends React.Component {
+interface ISquaresState {
+    squares: string[];
+}
+
+interface IGameState {
+    history: ISquaresState[];
+    xIsNext: boolean;
+}
+
+export class Game extends React.Component<{}, IGameState> {
+
+    constructor(props: any) {
+        super(props);
+        this.state = {
+          history: [{
+            squares: Array(9).fill(null),
+          }],
+          xIsNext: true,
+        };
+    }
+
     public render() {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const winner = this.calculateWinner(current.squares);
+
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+
         return (<div className="game">
             <div className="game-board">
-                <Board />
+                <Board
+                    squares={current.squares}
+                    // tslint:disable-next-line:jsx-no-lambda
+                    onClick={i => this.handleClick(i)} />
             </div>
             <div className="game-info">
-                <div />
+                <div>{status}</div>
                 <ol />
             </div>
         </div>);
+    }
+
+    private calculateWinner(squares: string[]): string | null {
+        const lines = [
+          [0, 1, 2],
+          [3, 4, 5],
+          [6, 7, 8],
+          [0, 3, 6],
+          [1, 4, 7],
+          [2, 5, 8],
+          [0, 4, 8],
+          [2, 4, 6],
+        ];
+        for (const line of lines) {
+          const [a, b, c] = line;
+          if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+          }
+        }
+        return null;
+    }
+
+    private handleClick(i: number) {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+        if (this.calculateWinner(squares) || squares[i]) {
+          return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+          history: history.concat([{squares}]),
+          xIsNext: !this.state.xIsNext,
+        });
     }
 }
