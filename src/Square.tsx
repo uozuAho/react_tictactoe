0fe47222e317d59ddcb03e484b024b19ced2a169
Warwick Stone <uozu.aho@gmail.com>
2018-11-17 17:38:15 +1100

    add square state + handler

--- src/Square.tsx
+++ src/Square.tsx
@@ -4,12 +4,26 @@ interface ISquareProps {
     value: number;
 }
 
-export class Square extends React.Component<ISquareProps> {
+interface ISquareState {
+    value: string;
+}
+
+export class Square extends React.Component<ISquareProps, ISquareState> {
+
+    constructor(props: ISquareProps) {
+        super(props);
+        this.state = {
+            value: ''
+        };
+    }
+
     public render() {
         return (
-            <button className="square">
-                {this.props.value}
+            <button className="square" onClick={this.onclick}>
+                {this.state.value}
             </button>
         );
     }
+
+    private onclick = () => this.setState({value: 'X'});
 }
